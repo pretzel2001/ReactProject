54b3fa7868f4259a6b85761845af46996d0d360f
Anannya Preeta <73926990+pretzel2001@users.noreply.github.com>
2024-06-04 15:30:42 +0600

    second commit

--- mui-demo/src/App.jsx
+++ mui-demo/src/App.jsx
@@ -1,21 +1,55 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Stack from '@mui/material/Stack';
+// import Stack from '@mui/material/Stack';
+// import Button from '@mui/material/Button';
+// import TextField from '@mui/material/TextField';
+// import Checkbox from '@mui/material/Checkbox';
+import Typography from '@mui/material/Typography';
+import SendIcon from '@mui/icons-material/Send';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+// import { makeStyles } from '@mui/system';
 import './App.css'
-
+import { borderColor, margin } from '@mui/system';
 
 function App() {
+  const classes= {
+    btn:{
+      fontSize:20,
+      backgroundColor:"pink",
+      borderColor: "pink",
+      color: "white",
+      '&:hover': {
+        backgroundColor: "violet",
+        borderColor: "violet",
+      }
+    },
+    body:{
+      padding:3
+    },
+    title:{
+      marginBottom: 2
+    }
+  }
   return (
     <>
-    <Stack>
+    {/* <Stack spacing={2} direction="row">
     <Button variant="text">Text</Button>
-    <Button variant="contained">Contained</Button>
-    <Button variant="outlined">Outlined</Button>
+    <Button variant="contained" disableElevation>Contained</Button>
+    <Button variant="outlined" disableRipple>Outlined</Button>
     <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    </Stack>
+    <Checkbox {...label} />
+    </Stack> */}
+    <Container sx={classes.body}>
+    <Typography sx={classes.title} variant="h5" color="textSecondary" align="left" noWrap gutterBottom >
+        Create New Note
+      </Typography>
+      <Button onClick={() => console.log('clicked')} sx = {classes.btn}variant="outlined" endIcon={<ArrowForwardIosIcon/>}>
+        Send
+      </Button>
+    </Container>
     </>
   )
 }
